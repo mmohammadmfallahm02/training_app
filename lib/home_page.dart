921d6186089b182145236edb10d3357aa930eda2
mmohammadmfallahm02 <mmohammadmfallahm02@gmail.com>
2023-07-03 15:37:17 +0330

    add get package(change materialapp to getmaterialapp)

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:training_app/colors.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage ({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(backgroundColor: AppColor.homePageBackground,);
   }
 }
