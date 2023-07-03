97fc2753d984236cfc66f37447b73ab38c3e577a
mmohammadmfallahm02 <mmohammadmfallahm02@gmail.com>
2023-07-03 16:14:08 +0330

    create first row homepage

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,10 +2,53 @@ import 'package:flutter/material.dart';
 import 'package:training_app/colors.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage ({super.key});
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: AppColor.homePageBackground,);
+    return Scaffold(
+      backgroundColor: AppColor.homePageBackground,
+      body: Container(
+        padding: const EdgeInsets.only(top: 20, left: 30, right: 30),
+        child: SafeArea(
+            child: Column(
+          children: [
+            Row(
+              children: [
+                Text(
+                  'Training',
+                  style: TextStyle(
+                      fontSize: 30,
+                      color: AppColor.homePageTitle,
+                      fontWeight: FontWeight.w700),
+                ),
+                const Spacer(),
+                Icon(
+                  Icons.arrow_back_ios,
+                  size: 20,
+                  color: AppColor.homePageIcons,
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Icon(
+                  Icons.calendar_today_outlined,
+                  size: 20,
+                  color: AppColor.homePageIcons,
+                ),
+                const SizedBox(
+                  width: 15,
+                ),
+                Icon(
+                  Icons.arrow_forward_ios,
+                  size: 20,
+                  color: AppColor.homePageIcons,
+                ),
+              ],
+            )
+          ],
+        )),
+      ),
+    );
   }
 }
