0ad3bc966f29890ce4bfbe76edd7f0ec29c35851
mmohammadmfallahm02 <mmohammadmfallahm02@gmail.com>
2023-07-03 15:26:37 +0330

    add assets files

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,22 +1,22 @@
-import 'package:flutter/material.dart';
+import 'dart:ui';
 
 class AppColor {
-  AppColor._();
-
-  static Color homePageBackground = const Color(0xfffafcfd);
-  static Color gradientFirst = const Color(0xff0c17aa);
-  static Color gradientSecond = const Color(0xff567bee);
-  static Color homePageTitle = const Color(0xff2a294d);
-  static Color homePageSubtitle = const Color(0xff3a3959);
-  static Color homePageDetail = const Color(0xff517efd);
-  static Color homePageIcons = const Color(0xff323453);
-  static Color homePageContainerTextBig = const Color(0xffffffff);
-  static Color homePagePlanColor = const Color(0xff9899a8);
-  static Color secondPageTopIconColor = const Color(0xffaeb4e7);
-  static Color secondPageTitleColor = const Color(0xfffefefe);
-  static Color secondPageContainerGradientFirstColor = const Color(0xff516ee0);
-  static Color secondPageContainerGradientSecondColor = const Color(0xfff7fbff);
-  static Color secondPageIconColor = const Color(0xff5784ee);
-  static Color setsColor = const Color(0xff8d8e9e);
-  static Color circuitsColor = const Color(0xff2a2a4a);
+  static Color homePageBackground = const Color(0xFFfbfcff);
+  static Color gradientFirst = const Color(0xff0f17ad);
+  static Color gradientSecond = const Color(0xFF6985e8);
+  static Color homePageTitle = const Color(0xFF302f51);
+  static Color homePageSubtitle = const Color(0xFF414160);
+  static Color homePageDetail = const Color(0xFF6588f4);
+  static Color homePageIcons = const Color(0xFF3b3c5c);
+  static Color homePageContainerTextSmall = const Color(0xFFf4f5fd);
+  static Color homePageContainerTextBig = const Color(0xFFffffff);
+  static Color homePagePlanColor = const Color(0xFFa2a2b1);
+  static Color secondPageTopIconColor = const Color(0xFFb7bce8);
+  static Color secondPageTitleColor = const Color(0xFFfefeff);
+  static Color secondPageContainerGradient1stColor = const Color(0xFF5564d8);
+  static Color secondPageContainerGradient2ndColor = const Color(0xFF6279dc);
+  static Color secondPageIconColor = const Color(0xFFfafafe);
+  static Color loopColor = const Color(0xFF6d8dea);
+  static Color setsColor = const Color(0xFF9999a9);
+  static Color circuitsColor = const Color(0xFF2f2f51);
 }
