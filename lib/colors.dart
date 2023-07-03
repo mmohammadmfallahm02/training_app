0ec44a53354019ac4472339cbea83a3b16b50475
mmohammadmfallahm02 <mmohammadmfallahm02@gmail.com>
2023-07-03 10:41:36 +0330

    add appcolor class

---
+++ lib/colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  AppColor._();
+
+  static Color homePageBackground = const Color(0xfffafcfd);
+  static Color gradientFirst = const Color(0xff0c17aa);
+  static Color gradientSecond = const Color(0xff567bee);
+  static Color homePageTitle = const Color(0xff2a294d);
+  static Color homePageSubtitle = const Color(0xff3a3959);
+  static Color homePageDetail = const Color(0xff517efd);
+  static Color homePageIcons = const Color(0xff323453);
+  static Color homePageContainerTextBig = const Color(0xffffffff);
+  static Color homePagePlanColor = const Color(0xff9899a8);
+  static Color secondPageTopIconColor = const Color(0xffaeb4e7);
+  static Color secondPageTitleColor = const Color(0xfffefefe);
+  static Color secondPageContainerGradientFirstColor = const Color(0xff516ee0);
+  static Color secondPageContainerGradientSecondColor = const Color(0xfff7fbff);
+  static Color secondPageIconColor = const Color(0xff5784ee);
+  static Color setsColor = const Color(0xff8d8e9e);
+  static Color circuitsColor = const Color(0xff2a2a4a);
+}
