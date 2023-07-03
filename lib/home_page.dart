ac607211f7a2890562b7ef2080b6654b401f3cd3
mmohammadmfallahm02 <mmohammadmfallahm02@gmail.com>
2023-07-03 19:01:47 +0330

    create second poster homepage

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -166,6 +166,82 @@ class HomePage extends StatelessWidget {
                       ],
                     )
                   ]),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            Container(
+              height: 180,
+              width: MediaQuery.sizeOf(context).width,
+              child: Stack(
+                children: [
+                  Container(
+                      margin: const EdgeInsets.only(top: 30),
+                      height: 120,
+                      width: MediaQuery.sizeOf(context).width,
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                              blurRadius: 40,
+                              offset: const Offset(8, 10),
+                              color: AppColor.gradientSecond.withOpacity(0.3)),
+                          BoxShadow(
+                              blurRadius: 10,
+                              offset: const Offset(-1, -5),
+                              color: AppColor.gradientSecond.withOpacity(0.3))
+                        ],
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(20)),
+                        image: const DecorationImage(
+                          fit: BoxFit.fill,
+                          image: AssetImage(
+                            'assets/images/card.jpg',
+                          ),
+                        ),
+                      )),
+                  Container(
+                      height: 200,
+                      width: MediaQuery.sizeOf(context).width,
+                      margin: const EdgeInsets.only(right: 200, bottom: 30),
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        image: DecorationImage(
+                          image: AssetImage(
+                            'assets/images/figure.png',
+                          ),
+                        ),
+                      )),
+                  Container(
+                    width: double.infinity,
+                    height: 100,
+                    margin: const EdgeInsets.only(left: 150, top: 50),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'You are doing great',
+                            style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: AppColor.homePageDetail),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          RichText(
+                            text: TextSpan(
+                                text: 'Keep it up\n',
+                                style: TextStyle(
+                                    color: AppColor.homePagePlanColor,
+                                    fontSize: 16),
+                                children: const [
+                                  TextSpan(text: 'stick to your plan')
+                                ]),
+                          ),
+                        ]),
+                  )
+                ],
+              ),
             )
           ],
         )),
