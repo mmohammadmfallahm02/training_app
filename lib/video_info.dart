2c3596f17cbe2ebef62520e056dab9e187606bf3
mmohammadmfallahm02 <mmohammadmfallahm02@gmail.com>
2023-07-04 21:40:14 +0330

    add detailwidget top section videoinfo page

--- lib/video_info.dart
+++ lib/video_info.dart
@@ -26,7 +26,6 @@ class _VideoInfoState extends State<VideoInfo> {
             children: [
               Container(
                 padding: const EdgeInsets.only(left: 30, right: 30, top: 50),
-                color: Colors.redAccent.withOpacity(0.5),
                 width: MediaQuery.sizeOf(context).width,
                 height: 300,
                 child: Column(
@@ -55,6 +54,78 @@ class _VideoInfoState extends State<VideoInfo> {
                         style: TextStyle(
                             fontSize: 25, color: AppColor.secondPageTitleColor),
                       ),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            width: 90,
+                            height: 30,
+                            decoration: BoxDecoration(
+                                borderRadius:
+                                    const BorderRadius.all(Radius.circular(10)),
+                                gradient: LinearGradient(
+                                    begin: Alignment.bottomLeft,
+                                    end: Alignment.topRight,
+                                    colors: [
+                                      AppColor
+                                          .secondPageContainerGradient1stColor,
+                                      AppColor
+                                          .secondPageContainerGradient2ndColor
+                                    ])),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Icon(
+                                  Icons.timer_outlined,
+                                  color: AppColor.secondPageIconColor,
+                                  size: 20,
+                                ),
+                                Text(
+                                  '68 min',
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      color: AppColor.secondPageIconColor),
+                                )
+                              ],
+                            ),
+                          ),
+                          Container(
+                            width: 240,
+                            height: 30,
+                            decoration: BoxDecoration(
+                                borderRadius:
+                                    const BorderRadius.all(Radius.circular(10)),
+                                gradient: LinearGradient(
+                                    begin: Alignment.bottomLeft,
+                                    end: Alignment.topRight,
+                                    colors: [
+                                      AppColor
+                                          .secondPageContainerGradient1stColor,
+                                      AppColor
+                                          .secondPageContainerGradient2ndColor
+                                    ])),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Icon(
+                                  Icons.handyman_outlined,
+                                  color: AppColor.secondPageIconColor,
+                                  size: 20,
+                                ),
+                                Text(
+                                  'Resistent band, Kettebell',
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      color: AppColor.secondPageIconColor),
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      )
                     ]),
               ),
             ],
