0defb73d2f6801954c7cae21026612f5338cdc52
mmohammadmfallahm02 <mmohammadmfallahm02@gmail.com>
2023-07-06 16:27:48 +0330

    fix videoinfo listview item

--- lib/video_info.dart
+++ lib/video_info.dart
@@ -12,12 +12,12 @@ class VideoInfo extends StatefulWidget {
 }
 
 class _VideoInfoState extends State<VideoInfo> {
-  List VideoInfo = [];
+  List videoInfo = [];
   _initData() async {
     await DefaultAssetBundle.of(context)
         .loadString('json/videoinfo.json')
         .then((value) {
-      VideoInfo = json.decode(value);
+      videoInfo = json.decode(value);
       setState(() {});
     });
   }
@@ -193,19 +193,20 @@ class _VideoInfoState extends State<VideoInfo> {
                     ),
                     Expanded(
                         child: ListView.builder(
-                      itemCount: VideoInfo.length,
+                      itemCount: videoInfo.length,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 30, vertical: 8),
                       itemBuilder: (BuildContext context, int index) {
-                        final item = VideoInfo[index];
+                        final item = videoInfo[index];
                         return GestureDetector(
                             onTap: () {
                               debugPrint(index.toString());
                             },
-                            child: Container(
-                              
+                            child: SizedBox(
                               height: 135,
                               width: 200,
-                              margin: const EdgeInsets.all(8),
-                              child: Column(mainAxisAlignment: MainAxisAlignment.center,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
                                 children: [
                                   Row(
                                     children: [
@@ -214,12 +215,13 @@ class _VideoInfoState extends State<VideoInfo> {
                                         height: 80,
                                         decoration: BoxDecoration(
                                             borderRadius:
-                                                BorderRadius.circular(10),
+                                                BorderRadius.circular(15),
                                             image: DecorationImage(
                                                 image: AssetImage(
-                                                    item['thumbnail']))),
+                                                    item['thumbnail']),
+                                                fit: BoxFit.cover)),
                                       ),
-                                      SizedBox(
+                                      const SizedBox(
                                         width: 10,
                                       ),
                                       Column(
@@ -232,7 +234,7 @@ class _VideoInfoState extends State<VideoInfo> {
                                                 fontSize: 18,
                                                 fontWeight: FontWeight.bold),
                                           ),
-                                          SizedBox(
+                                          const SizedBox(
                                             height: 10,
                                           ),
                                           Text(
